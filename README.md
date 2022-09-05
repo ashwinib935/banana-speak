8d877853e6330e1eced00160d82822a3ce333908
ashwinib935 <110533153+ashwinib935@users.noreply.github.com>
2022-09-05 12:01:33 +0530

    Feat: application with api

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# banana-speak
- LessonFour-MarkSix
+# Minion Translator
+It is application that traslate our English sentence to a Minion sentence.
+It uses API to fetch minion language.
+
+
+# Tech stack used:
+ HTML
+ CSS
+ Javascript
+
+# Hosted on
+Netlify
+
+# Preview
+ 
+
+ 
---
+++ app.js
@@ -0,0 +1,28 @@
+var btn_translate = document.querySelector("#btn-translate");
+var txt_input = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+btn_translate.addEventListener("click", clickEventHandler);
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("Error occured:", error);
+    alert("Something wrong with server! Please try again after sometime");
+}
+
+function clickEventHandler() {
+    var inputText = txt_input.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        }).catch(errorHandler);
+
+}
