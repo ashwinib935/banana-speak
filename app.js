d2e720e74d3e9b973469d81ea17c9afcf180df4c
ashwinib935 <110533153+ashwinib935@users.noreply.github.com>
2022-09-08 14:28:12 +0530

    fix: empty value

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var btn_translate = document.querySelector("#btn-translate");
 var txt_input = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var message = document.querySelector("#message");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
@@ -18,11 +19,18 @@ function errorHandler(error) {
 
 function clickEventHandler() {
     var inputText = txt_input.value;
-    fetch(getTranslationURL(inputText))
-        .then(response => response.json())
-        .then(json => {
-            var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText;
-        }).catch(errorHandler);
+    if (inputText) {
+        message.style.display = "none";
+        fetch(getTranslationURL(inputText))
+            .then(response => response.json())
+            .then(json => {
+                var translatedText = json.contents.translated;
+                outputDiv.innerText = translatedText;
+            }).catch(errorHandler);
+    } else {
+        message.style.display = "block";
+        message.innerText = "Please write something to translate."
+    }
+
 
 }
